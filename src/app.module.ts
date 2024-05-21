e00b5b894b93b1a75c9daa16cacb8dbff59f4144
suraj <suraj@funnelstory.io>
2024-05-21 17:10:21 +0530

    chore: add mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { DatabaseModule } from './common/database/database.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
