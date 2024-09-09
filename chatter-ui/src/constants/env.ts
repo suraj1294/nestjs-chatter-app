369d3688476fd1998ba00ecc01587f452b7313d4
ipalace <ipalace@ipalaces-MacBook-Pro.local>
2024-09-09 11:27:15 +0530

    fix secure websocket connection

--- chatter-ui/src/constants/env.ts
+++ chatter-ui/src/constants/env.ts
@@ -1,3 +1,3 @@
 export const GRAPHQL_API_URL = `${window.location.origin}/graphql`;
 export const AUTH_API_URL = `${window.location.origin}/api/auth`;
-export const GRAPHQL_WS_URL = `${process.env.NODE_ENV === 'development' ? 'ws://localhost:3000' : 'wss://' + window.location.host}}/graphql`;
+export const GRAPHQL_WS_URL = `${process.env.NODE_ENV === 'development' ? 'ws://localhost:3000' : 'wss://' + window.location.host}/graphql`;
